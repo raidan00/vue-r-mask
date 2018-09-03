5de5e521212c236eea7a0d7a2f7dab4c75c18d3b
Sergei <raidan00@mail.ru>
2018-09-03 20:00:44 +0300

    set tabs as tab

--- vue-r-mask/index.js
+++ vue-r-mask/index.js
@@ -1,8 +1,8 @@
 import caret from  './caretPos.js';
 
 export default {
-  bind (el, val, VNode){
-    bind(...arguments)
+	bind (el, val, VNode){
+		bind(...arguments)
   },
 	update (el, val, VNode){
 		if( val.value.toString() == val.oldValue.toString()) return;
@@ -27,7 +27,7 @@ function initMask (el, val){
 		if(match[0].length <= 2){
 			toPush.minLen = 1;
 			toPush.maxLen = 1;
-      toPush.type = 'single';
+			toPush.type = 'single';
       toPush.char = match[0][match[0].length-1];
       toPush.reg = new RegExp(match[0]+'+');
 		} else {
@@ -37,55 +37,55 @@ function initMask (el, val){
 		}
 		frame.push(toPush);
 	}
-  return function (){
+	return function (){
 		//let forTests = { before: this.value.slice(0, caret.get(this)) + '|' + this.value.slice( caret.get(this) )};
 
-    if (this.value === '' && this.placeholder) { return }
+		if (this.value === '' && this.placeholder) { return }
 
-    let arr = this.value.split('').map((e)=>{return {char: e, type: 'char'}});
-    let pos = { char: '', type: 'pos' };
-    arr.splice(caret.get(this), 0, pos);
-    let newVal = [];
-    mainLoop: for(let i = 0; i < frame.length; i++){
-      for(let k = 0; k < frame[i].maxLen; k++){
-        if(arr[0] && arr[0].type == 'pos'){
-          newVal.push(arr.shift());
-          k--;
-          continue;
-        }
-        if(arr[0] == undefined){
-          if(frame[i].type == 'single'){
-            newVal.push({char:frame[i].char});
-          }else{
+		let arr = this.value.split('').map((e)=>{return {char: e, type: 'char'}});
+		let pos = { char: '', type: 'pos' };
+		arr.splice(caret.get(this), 0, pos);
+		let newVal = [];
+		mainLoop: for(let i = 0; i < frame.length; i++){
+			for(let k = 0; k < frame[i].maxLen; k++){
+				if(arr[0] && arr[0].type == 'pos'){
+					newVal.push(arr.shift());
+					k--;
+					continue;
+				}
+				if(arr[0] == undefined){
+					if(frame[i].type == 'single'){
+						newVal.push({char:frame[i].char});
+					}else{
 						if( k >= frame[i].minLen ){
 							continue mainLoop;
 						}
-            newVal.push({char:'_'});
-          }
-          continue;
-        }
+						newVal.push({char:'_'});
+					}
+					continue;
+				}
         if(frame[i].reg.exec(arr[0].char)){
 					if( k >= frame[i].minLen && arr[0].char == '_') {
 						continue mainLoop;
 					}
           newVal.push(arr.shift());
         }else{
-          if(frame[i].type == 'single'){
-              newVal.push({char:frame[i].char});
-          }else{
-							if( k >= frame[i].minLen ) {
-								continue mainLoop;
-							}
-              arr.shift();
-              k--;
-          }
-        }
-      }
-    }
-    this.value = newVal.map((e)=>e.char).join('');
-    caret.set(this, newVal.indexOf(pos));
+					if(frame[i].type == 'single'){
+						newVal.push({char:frame[i].char});
+					}else{
+						if( k >= frame[i].minLen ) {
+							continue mainLoop;
+						}
+						arr.shift();
+						k--;
+					}
+				}
+			}
+		}
+		this.value = newVal.map((e)=>e.char).join('');
+		caret.set(this, newVal.indexOf(pos));
 
 		//forTests.after = this.value.slice(0, caret.get(this)) + '|' + this.value.slice( caret.get(this) );
 		//console.log(forTests);
-  }
+	}
 };
